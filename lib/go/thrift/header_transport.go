7d22ef721816ef10d49f396f7310a5f279b2dff9
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2019-06-07 20:47:18 +0800

    WIP: THRIFT-4612: THeader support in go library
    Implement THeaderTransport and THeaderProtocol, with support of:
    
    * clients:
      - headers
      - framedBinary
      - unframedBinary
      - framedCompact
      - unframedCompact
    * transforms:
      - none
      - zlib
    * info types:
      - key_value
    * subprotocols:
      - TBinary
      - TCompact
    
    The support list is in general on par of the THeader implementation in
    python library.

---
+++ lib/go/thrift/header_transport.go
@@ -0,0 +1,643 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package thrift
+
+import (
+	"bufio"
+	"bytes"
+	"compress/zlib"
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+// Size in bytes for 32-bin ints.
+const size32 = 4
+
+type headerMeta struct {
+	MagicFlags   uint32
+	SequenceID   int32
+	HeaderLength uint16
+}
+
+const headerMetaSize = 10
+
+// Constants defined in THeader format:
+// https://github.com/apache/thrift/blob/master/doc/specs/HeaderFormat.md
+const (
+	THeaderHeaderMagic  uint32 = 0x0FFF0000
+	THeaderHeaderMask   uint32 = 0xFFFF0000
+	THeaderFlagsMask    uint32 = 0x0000FFFF
+	THeaderMaxFrameSize uint32 = 0x3FFFFFFF
+)
+
+// THeaderMap is the type of the header map in THeader transport.
+type THeaderMap map[string]string
+
+type clientType int
+
+const (
+	clientUnknown clientType = iota
+	clientHeaders
+	clientFramedBinary
+	clientUnframedBinary
+	clientFramedCompact
+	clientUnframedCompact
+)
+
+// THeaderSubprotocolID is the subprotocol id used in THeader protocol.
+type THeaderSubprotocolID int64
+
+// Supported THeaderSubprotocolID values.
+const (
+	THeaderSubprotocolBinary  THeaderSubprotocolID = 0x00
+	THeaderSubprotocolCompact THeaderSubprotocolID = 0x02
+	THeaderSubprotocolDefault                      = THeaderSubprotocolBinary
+)
+
+// GetProtocol gets the corresponding TProtocol from the subprotocol id.
+func (id THeaderSubprotocolID) GetProtocol(trans TTransport) (TProtocol, error) {
+	switch id {
+	default:
+		return nil, NewTProtocolExceptionWithType(
+			NOT_IMPLEMENTED,
+			fmt.Errorf("THeader subprotocol id %d not supported", id),
+		)
+	case THeaderSubprotocolBinary:
+		return NewTBinaryProtocolFactoryDefault().GetProtocol(trans), nil
+	case THeaderSubprotocolCompact:
+		return NewTCompactProtocol(trans), nil
+	}
+}
+
+// THeaderTransformID defines the numeric id of the transform used.
+type THeaderTransformID int
+
+// THeaderTransformID values
+const (
+	TransformNone THeaderTransformID = iota // 0, no special handling
+	TransformZlib                           // 1, zlib
+	// Rest of the values are not currently supported, namely HMAC and Snappy.
+)
+
+var supportedTransformIDs = map[THeaderTransformID]bool{
+	TransformNone: true,
+	TransformZlib: true,
+}
+
+// ReadTransform transform the data stream for read.
+func (id THeaderTransformID) ReadTransform(r io.Reader) (io.ReadCloser, error) {
+	switch id {
+	default:
+		return nil, NewTProtocolExceptionWithType(
+			NOT_IMPLEMENTED,
+			fmt.Errorf("THeaderTransformID %d not supported", id),
+		)
+	case TransformNone:
+		// Wrap it with ioutil.NopCloser is important because it makes
+		// sure that the Close function on the returned io.ReadCloser is
+		// a noop, so it's safe to be pushed into closerStack without
+		// worrying about double closing.
+		return ioutil.NopCloser(r), nil
+	case TransformZlib:
+		// closerStack in THeaderTransport is needed because
+		// zlib.Reader.Close does not call the Close function of the
+		// underlying ReadCloser.
+		return zlib.NewReader(r)
+	}
+}
+
+// Like ioutil.nopCloser, but for writers.
+type writerWrapper struct {
+	io.Writer
+}
+
+func (writerWrapper) Close() error {
+	return nil
+}
+
+// WriteTransform transform the data stream for write.
+func (id THeaderTransformID) WriteTransform(w io.Writer) (io.WriteCloser, error) {
+	switch id {
+	default:
+		return nil, NewTProtocolExceptionWithType(
+			NOT_IMPLEMENTED,
+			fmt.Errorf("THeaderTransformID %d not supported", id),
+		)
+	case TransformNone:
+		return writerWrapper{w}, nil
+	case TransformZlib:
+		return zlib.NewWriter(w), nil
+	}
+}
+
+// THeaderInfoType is the type id of the info headers.
+type THeaderInfoType int
+
+// Supported THeaderInfoType values.
+const (
+	_            THeaderInfoType = iota // skip 0
+	InfoKeyValue                        // 1
+	// Rest of the info types are not supported.
+)
+
+// THeaderTransport is a Transport mode that implements THeader.
+//
+// Note that THeaderTransport handles frame and zlib by itself,
+// so the underlying transport should be a raw socket transports (TSocket or TSSLSocket),
+// instead of rich transports like TZlibTransport or TFramedTransport.
+type THeaderTransport struct {
+	SequenceID int32
+	Flags      uint32
+
+	transport TTransport
+
+	reader *bufio.Reader
+	// frameReader should be a bufio.Reader wrapped io.LimitedReader (with
+	// possible other wrappers, like zlib.Reader, in between). The limit on
+	// LimitedReader would be the frame size so at the end of frame it will
+	// return EOF error and we should start reading the next frame.
+	// When frameReader is nil, reader should be used for reading.
+	frameReader *bufio.Reader
+	// Closers to be called at the end of the frame reading.
+	readClosers []io.Closer
+
+	writeBuffer     bytes.Buffer
+	writeTransforms []THeaderTransformID
+
+	clientType clientType
+	protocolID THeaderSubprotocolID
+
+	// THeaderMap for read and write
+	readHeaders  THeaderMap
+	writeHeaders THeaderMap
+}
+
+var _ TTransport = (*THeaderTransport)(nil)
+
+// NewTHeaderTransport creates THeaderTransport from the underlying transport.
+//
+// Please note that THeaderTransport handles framing and zlib by itself,
+// so the underlying transport should be the raw socket transports (TSocket or TSSLSocket),
+// instead of rich transports like TZlibTransport or TFramedTransport.
+func NewTHeaderTransport(trans TTransport) *THeaderTransport {
+	return &THeaderTransport{
+		transport:    trans,
+		reader:       bufio.NewReader(trans),
+		writeHeaders: make(THeaderMap),
+		protocolID:   THeaderSubprotocolDefault,
+	}
+}
+
+// Open calls the underlying transport's Open function.
+func (t *THeaderTransport) Open() error {
+	return t.transport.Open()
+}
+
+// IsOpen calls the underlying transport's IsOpen function.
+func (t *THeaderTransport) IsOpen() bool {
+	return t.transport.IsOpen()
+}
+
+// ReadFrame tries to read the frame header, guess the client type, and handle
+// unframed clients.
+func (t *THeaderTransport) ReadFrame() error {
+	if !t.needReadFrame() {
+		// No need to read frame, skipping.
+		return nil
+	}
+	// Peek and handle the first 32 bits.
+	// They could either be the length field of a framed message,
+	// or the first bytes of an unframed message.
+	buf, err := t.reader.Peek(size32)
+	if err != nil {
+		return err
+	}
+	frameSize := binary.BigEndian.Uint32(buf)
+	if frameSize&VERSION_MASK == VERSION_1 {
+		t.clientType = clientUnframedBinary
+		return nil
+	}
+	if buf[0] == COMPACT_PROTOCOL_ID && buf[1]&COMPACT_VERSION_MASK == COMPACT_VERSION {
+		t.clientType = clientUnframedCompact
+		return nil
+	}
+
+	// Now we are somewhat certain that it should be a framed message,
+	// sanity check on frameSize then discard the peeked part.
+	if frameSize > THeaderMaxFrameSize {
+		return NewTProtocolExceptionWithType(
+			SIZE_LIMIT,
+			errors.New("frame too large"),
+		)
+	}
+	t.reader.Discard(size32)
+
+	t.frameReader = bufio.NewReader(io.LimitReader(t.reader, int64(frameSize)))
+
+	// Peek and handle the next 32 bits.
+	buf, err = t.frameReader.Peek(size32)
+	if err != nil {
+		return err
+	}
+	version := binary.BigEndian.Uint32(buf)
+	if version&THeaderHeaderMask == THeaderHeaderMagic {
+		t.clientType = clientHeaders
+		return t.parseHeaders(frameSize)
+	}
+	if version&VERSION_MASK == VERSION_1 {
+		t.clientType = clientFramedBinary
+		return nil
+	}
+	if buf[0] == COMPACT_PROTOCOL_ID && buf[1]&COMPACT_VERSION_MASK == COMPACT_VERSION {
+		t.clientType = clientFramedCompact
+		return nil
+	}
+	t.frameReader = nil
+	return NewTProtocolExceptionWithType(
+		NOT_IMPLEMENTED,
+		errors.New("unsupported client transport type"),
+	)
+}
+
+func (t *THeaderTransport) parseHeaders(frameSize uint32) error {
+	if t.clientType != clientHeaders {
+		return nil
+	}
+
+	var err error
+	var meta headerMeta
+	if err = binary.Read(t.frameReader, binary.BigEndian, &meta); err != nil {
+		return err
+	}
+	frameSize -= headerMetaSize
+	t.Flags = meta.MagicFlags & THeaderFlagsMask
+	t.SequenceID = meta.SequenceID
+	headerLength := int64(meta.HeaderLength) * 4
+	if int64(frameSize) < headerLength {
+		return NewTProtocolExceptionWithType(
+			SIZE_LIMIT,
+			errors.New("header size is larger than the whole frame"),
+		)
+	}
+	var headerBuf bytes.Buffer
+	_, err = io.Copy(&headerBuf, io.LimitReader(t.frameReader, headerLength))
+	if err != nil {
+		return err
+	}
+
+	// At this point the header is already read into headerBuf,
+	// and t.frameReader starts from the actual payload.
+	headerReader := bufio.NewReader(&headerBuf)
+	protoID, err := binary.ReadVarint(headerReader)
+	if err != nil {
+		return err
+	}
+	t.protocolID = THeaderSubprotocolID(protoID)
+	var transformCount int64
+	transformCount, err = binary.ReadVarint(headerReader)
+	if err != nil {
+		return err
+	}
+	if transformCount > 0 {
+		t.readClosers = make([]io.Closer, transformCount)
+		transformIDs := make([]THeaderTransformID, transformCount)
+		for i := 0; i < int(transformCount); i++ {
+			id, err := binary.ReadVarint(headerReader)
+			if err != nil {
+				return err
+			}
+			transformIDs[i] = THeaderTransformID(id)
+		}
+		var reader io.Reader = t.frameReader
+		// The transform IDs on the wire was added based on the order of
+		// writing, so on the reading side we need to reverse the order.
+		for i := transformCount - 1; i >= 0; i-- {
+			id := transformIDs[i]
+			wrapped, err := id.ReadTransform(reader)
+			if err != nil {
+				return err
+			}
+			reader = wrapped
+			// closers are added in the reverse order, so when we
+			// close them on the normal order it naturally works as
+			// a stack.
+			t.readClosers[i] = wrapped
+		}
+		t.frameReader = bufio.NewReader(reader)
+	} else {
+		t.readClosers = nil
+	}
+
+	// The info part does not use the transforms yet, so it's
+	// important to continue using headerReader.
+	headers := make(THeaderMap)
+	for {
+		infoType, err := binary.ReadVarint(headerReader)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if THeaderInfoType(infoType) == InfoKeyValue {
+			count, err := binary.ReadVarint(headerReader)
+			if err != nil {
+				return err
+			}
+			for i := 0; i < int(count); i++ {
+				key, err := readString(headerReader)
+				if err != nil {
+					return err
+				}
+				value, err := readString(headerReader)
+				if err != nil {
+					return err
+				}
+				headers[key] = value
+			}
+		} else {
+			// Skip reading info section on the first
+			// unsupported info type.
+			break
+		}
+	}
+	t.readHeaders = headers
+
+	return nil
+}
+
+func (t *THeaderTransport) needReadFrame() bool {
+	if t.clientType == clientUnknown {
+		// This is a new connection that's never read before.
+		return true
+	}
+	if t.isFramed() && t.frameReader == nil {
+		// We just finished the last frame.
+		return true
+	}
+	return false
+}
+
+func (t *THeaderTransport) Read(p []byte) (int, error) {
+	if err := t.ReadFrame(); err != nil {
+		return 0, err
+	}
+	if t.frameReader != nil {
+		read, err := t.frameReader.Read(p)
+		if err == io.EOF {
+			t.frameReader = nil
+			for _, closer := range t.readClosers {
+				if err := closer.Close(); err != nil {
+					return read, err
+				}
+			}
+			if read < len(p) {
+				var nextRead int
+				nextRead, err = t.Read(p[read:])
+				read += nextRead
+			}
+		}
+		return read, err
+	}
+	return t.reader.Read(p)
+}
+
+// Write writes data to the write buffer. You need to call Flush at the end of
+// the message to actually write them to the transport.
+func (t *THeaderTransport) Write(p []byte) (int, error) {
+	return t.writeBuffer.Write(p)
+}
+
+// Flush writes the appropriate header and the write buffer to the underlying transport.
+func (t *THeaderTransport) Flush(ctx context.Context) error {
+	defer t.writeBuffer.Reset()
+
+	switch t.clientType {
+	default:
+		fallthrough
+	case clientUnknown:
+		t.clientType = clientHeaders
+		fallthrough
+	case clientHeaders:
+		var headers bytes.Buffer
+		if err := writeVarint(&headers, int64(t.protocolID)); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+		if err := writeVarint(&headers, int64(len(t.writeTransforms))); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+		for _, transform := range t.writeTransforms {
+			if err := writeVarint(&headers, int64(transform)); err != nil {
+				return NewTTransportExceptionFromError(err)
+			}
+		}
+		if len(t.writeHeaders) > 0 {
+			if err := writeVarint(&headers, int64(InfoKeyValue)); err != nil {
+				return NewTTransportExceptionFromError(err)
+			}
+			if err := writeVarint(&headers, int64(len(t.writeHeaders))); err != nil {
+				return NewTTransportExceptionFromError(err)
+			}
+			for key, value := range t.writeHeaders {
+				if err := writeString(&headers, key); err != nil {
+					return NewTTransportExceptionFromError(err)
+				}
+				if err := writeString(&headers, value); err != nil {
+					return NewTTransportExceptionFromError(err)
+				}
+			}
+		}
+		padding := 4 - headers.Len()%4
+		if padding < 4 {
+			buf := make([]byte, padding)
+			if _, err := headers.Write(buf); err != nil {
+				return NewTTransportExceptionFromError(err)
+			}
+		}
+
+		var payload bytes.Buffer
+		meta := headerMeta{
+			MagicFlags:   THeaderHeaderMagic + t.Flags&THeaderFlagsMask,
+			SequenceID:   t.SequenceID,
+			HeaderLength: uint16(headers.Len() / 4),
+		}
+		if err := binary.Write(&payload, binary.BigEndian, meta); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+		if _, err := io.Copy(&payload, &headers); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+
+		var writer io.Writer = &payload
+		closers := make([]io.Closer, len(t.writeTransforms))
+		for i, transform := range t.writeTransforms {
+			wrapped, err := transform.WriteTransform(writer)
+			if err != nil {
+				return NewTTransportExceptionFromError(err)
+			}
+			writer = wrapped
+			closers[i] = wrapped
+		}
+		if _, err := io.Copy(writer, &t.writeBuffer); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+		for i := len(closers) - 1; i >= 0; i-- {
+			// We need to close the closers in reverse order
+			if err := closers[i].Close(); err != nil {
+				return NewTTransportExceptionFromError(err)
+			}
+		}
+
+		// First write frame length
+		buf := make([]byte, size32)
+		binary.BigEndian.PutUint32(buf, uint32(payload.Len()))
+		if _, err := t.transport.Write(buf); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+		// Then write the payload
+		if _, err := io.Copy(t.transport, &payload); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+
+	case clientFramedBinary, clientFramedCompact:
+		buf := make([]byte, size32)
+		binary.BigEndian.PutUint32(buf, uint32(t.writeBuffer.Len()))
+		if _, err := t.transport.Write(buf); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+		fallthrough
+	case clientUnframedBinary, clientUnframedCompact:
+		if _, err := io.Copy(t.transport, &t.writeBuffer); err != nil {
+			return NewTTransportExceptionFromError(err)
+		}
+	}
+
+	select {
+	default:
+	case <-ctx.Done():
+		return NewTTransportExceptionFromError(ctx.Err())
+	}
+
+	return t.transport.Flush(ctx)
+}
+
+// Close closes the transport, along with its underlying transport.
+func (t *THeaderTransport) Close() error {
+	if err := t.Flush(context.Background()); err != nil {
+		return err
+	}
+	return t.transport.Close()
+}
+
+// RemainingBytes calls underlying transport's RemainingBytes.
+//
+// Even in framed cases, because of all the possible compression transforms
+// involved, the remaining frame size is likely to be different from the actual
+// remaining readable bytes, so we don't bother to keep tracking the remaining
+// frame size by ourselves and just use the underlying transport's
+// RemainingBytes directly.
+func (t *THeaderTransport) RemainingBytes() uint64 {
+	return t.transport.RemainingBytes()
+}
+
+// GetReadHeaders returns the THeaderMap read from transport.
+func (t *THeaderTransport) GetReadHeaders() THeaderMap {
+	return t.readHeaders
+}
+
+// SetWriteHeader sets a header for write.
+func (t *THeaderTransport) SetWriteHeader(key, value string) {
+	t.writeHeaders[key] = value
+}
+
+// ClearWriteHeaders clears all write headers previously set.
+func (t *THeaderTransport) ClearWriteHeaders() {
+	t.writeHeaders = make(THeaderMap)
+}
+
+// AddTransform add a transform for writing.
+func (t *THeaderTransport) AddTransform(transform THeaderTransformID) error {
+	if !supportedTransformIDs[transform] {
+		return NewTProtocolExceptionWithType(
+			NOT_IMPLEMENTED,
+			fmt.Errorf("THeaderTransformID %d not supported", transform),
+		)
+	}
+	t.writeTransforms = append(t.writeTransforms, transform)
+	return nil
+}
+
+// Protocol returns the subprotocol id used in this THeaderTransport.
+//
+// It's guaranteed to return a supported subprotocol id.
+func (t *THeaderTransport) Protocol() THeaderSubprotocolID {
+	switch t.clientType {
+	default:
+		return t.protocolID
+	case clientFramedBinary, clientUnframedBinary:
+		return THeaderSubprotocolBinary
+	case clientFramedCompact, clientUnframedCompact:
+		return THeaderSubprotocolCompact
+	}
+}
+
+func (t *THeaderTransport) isFramed() bool {
+	switch t.clientType {
+	default:
+		return false
+	case clientHeaders, clientFramedBinary, clientFramedCompact:
+		return true
+	}
+}
+
+func readString(r *bufio.Reader) (string, error) {
+	size, err := binary.ReadVarint(r)
+	if err != nil {
+		return "", err
+	}
+	if size < 0 {
+		return "", NewTProtocolExceptionWithType(
+			NEGATIVE_SIZE,
+			fmt.Errorf("negative size %d for string data", size),
+		)
+	}
+	buf := make([]byte, size)
+	read, err := r.Read(buf)
+	return string(buf[:read]), err
+}
+
+func writeVarint(w io.Writer, v int64) error {
+	var buf [binary.MaxVarintLen64]byte
+	written := binary.PutVarint(buf[:], v)
+	_, err := w.Write(buf[:written])
+	return NewTTransportExceptionFromError(err)
+}
+
+func writeString(w io.Writer, s string) error {
+	if err := writeVarint(w, int64(len(s))); err != nil {
+		return NewTTransportExceptionFromError(err)
+	}
+	_, err := w.Write([]byte(s))
+	return NewTTransportExceptionFromError(err)
+}
